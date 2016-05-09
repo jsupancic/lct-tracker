3f62cb182d821eeaf07bb0b191689e1077cf559d
James Steven Supancic III <arrummzen@gmail.com>
2016-05-09 11:02:04 -0700

    fix

--- run_tracker.m
+++ run_tracker.m
@@ -101,7 +101,7 @@ function [precision, fps, k_bbs] = run_tracker(video, show_visualization, show_p
 		
         
 		%call tracker function with all the relevant parameters
-		k_bbs = tracker_lct(video_path, img_files, pos, target_sz, config, show_visualization);        
+		[~,~,k_bbs] = tracker_lct(video_path, img_files, pos, target_sz, config, show_visualization);        
                 precision = nan;
                 fps = nan;
     end
